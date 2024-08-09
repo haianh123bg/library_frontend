cdf8dc571feda1f4c7bf80aec468bb8da2bbe96c
haianh123bg <nguyenngochaianh20033@gmail.com>
2024-08-09 14:17:18 +0700

    edit title

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
-                Xin chào mọi người
+                Xin chào
             </Button>
 
             <Modal show={show} onHide={handleClose}>
